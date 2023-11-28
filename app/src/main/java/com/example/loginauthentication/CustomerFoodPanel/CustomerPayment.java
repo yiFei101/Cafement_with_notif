16c0e7572df6f041dd28d1d53ab0de28beda367c
yiFei101 <114209594+yiFei101@users.noreply.github.com>
2023-11-29 07:26:11 +0800

    latest commit

--- app/src/main/java/com/example/loginauthentication/CustomerFoodPanel/CustomerPayment.java
+++ app/src/main/java/com/example/loginauthentication/CustomerFoodPanel/CustomerPayment.java
@@ -1,5 +1,8 @@
 package com.example.loginauthentication.CustomerFoodPanel;
 
+import static java.security.AccessController.getContext;
+
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,11 +15,16 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.loginauthentication.CustomerFoodPanel_BottomNavigation;
+import com.example.loginauthentication.MerchantPanel.ChefPreparedOrderView;
 import com.example.loginauthentication.R;
+import com.example.loginauthentication.ReusableCodeForAll;
 import com.example.loginauthentication.SendNotification.APIService;
 import com.example.loginauthentication.SendNotification.Data;
 import com.example.loginauthentication.SendNotification.MyResponse;
 import com.example.loginauthentication.SendNotification.NotificationSender;
+import com.example.loginauthentication.utilities.ApiClient;
+import com.example.loginauthentication.utilities.ApiService;
+import com.example.loginauthentication.utilities.Constants;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -26,6 +34,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 
@@ -37,7 +49,11 @@ import retrofit2.Response;
 public class CustomerPayment extends AppCompatActivity {
 
     TextView  CashPayment;
-    String RandomUID, MerchantId;
+    String RandomUID, MerchantId, currentUserToken,merchantToken,currentUserName,userID;
+    String  merchantID;
+    private ProgressDialog progressDialog;
+
+
     private APIService apiService;
 
     @Override
@@ -107,6 +123,42 @@ public class CustomerPayment extends AppCompatActivity {
                                                     hashMap2.put("UserId", customerPaymentOrderss.getUserId());
                                                     FirebaseDatabase.getInstance().getReference("MerchantWaitingOrders").child(MerchantId).child(RandomUID).child("Dishes").child(dishid).setValue(hashMap2);
                                                 }
+                                                //for user token
+                                                DatabaseReference db1 = FirebaseDatabase.getInstance().getReference("Tokens").child(userID);
+                                                db1.addListenerForSingleValueEvent(new ValueEventListener() {
+                                                    @Override
+                                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                        if (dataSnapshot.exists()) {
+                                                            currentUserToken = dataSnapshot.getValue(String.class);
+
+                                                        }
+                                                        else {
+                                                        }
+                                                    }
+                                                    @Override
+                                                    public void onCancelled(@NonNull DatabaseError error) {
+                                                        Toast.makeText(v.getContext(),"Something went wrong.",Toast.LENGTH_LONG).show();
+                                                    }
+                                                });
+
+                                                //for merchant token
+                                                DatabaseReference db2 = FirebaseDatabase.getInstance().getReference("Tokens").child(merchantID);
+                                                db2.addListenerForSingleValueEvent(new ValueEventListener() {
+                                                    @Override
+                                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                        if (dataSnapshot.exists()) {
+                                                            merchantToken = dataSnapshot.getValue(String.class);
+                                                        }
+                                                        else {
+                                                        }
+                                                    }
+                                                    @Override
+                                                    public void onCancelled(@NonNull DatabaseError error) {
+                                                        Toast.makeText(v.getContext(),"Something went wrong.",Toast.LENGTH_LONG).show();
+                                                    }
+                                                });
+
+
                                                 DatabaseReference dataa = FirebaseDatabase.getInstance().getReference("StudentPaymentOrders").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(RandomUID).child("OtherInformation");
                                                 dataa.addListenerForSingleValueEvent(new ValueEventListener() {
                                                     @Override
@@ -136,10 +188,31 @@ public class CustomerPayment extends AppCompatActivity {
                                                                                                 FirebaseDatabase.getInstance().getReference().child("Tokens").child(MerchantId).child("token").addListenerForSingleValueEvent(new ValueEventListener() {
                                                                                                     @Override
                                                                                                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                                                                                        String usertoken = dataSnapshot.getValue(String.class);
-                                                                                                        sendNotifications(usertoken, "Order Confirmed", "Payment mode is confirmed by user, Now you can start the order", "Confirm");
+                                                                                                        try{
+                                                                                                            JSONArray tokens = new JSONArray();
+                                                                                                            tokens.put(merchantToken);
 
+                                                                                                            JSONObject data = new JSONObject();
+                                                                                                            data.put(Constants.KEY_USER_ID, userID);
+                                                                                                            data.put(Constants.KEY_NAME, "New Order");
+                                                                                                            data.put(Constants.KEY_FCM_TOKEN, currentUserToken);
+                                                                                                            data.put(Constants.KEY_MESSAGE, "You have new order from "+currentUserName);
+
+                                                                                                            JSONObject body = new JSONObject();
+                                                                                                            body.put(Constants.REMOTE_MSG_DATA, data);
+                                                                                                            body.put(Constants.REMOTE_MSG_REGISTRATION_IDS, tokens);
+
+                                                                                                            sendNotification(body.toString());
+
+
+                                                                                                        }catch(Exception exception){
+                                                                                                            Toast.makeText(v.getContext(),"Something went wrong.",Toast.LENGTH_LONG).show();
+                                                                                                        }
+
+                                                                                                        progressDialog.dismiss();
+                                                                                                        ReusableCodeForAll.ShowAlert(v.getContext(), "", "Your Order has been Ready for Pickup ");
                                                                                                     }
+
 
                                                                                                     @Override
                                                                                                     public void onCancelled(@NonNull DatabaseError databaseError) {
@@ -148,27 +221,7 @@ public class CustomerPayment extends AppCompatActivity {
                                                                                                 });
 
                                                                                             }
-                                                                                        }).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                                                            @Override
-                                                                                            public void onSuccess(Void aVoid) {
-                                                                                                AlertDialog.Builder builder = new AlertDialog.Builder(CustomerPayment.this);
-                                                                                                builder.setMessage("Payment mode confirmed, Now you can track your order.");
-                                                                                                builder.setCancelable(false);
-                                                                                                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                                                                                    @Override
-                                                                                                    public void onClick(DialogInterface dialog, int which) {
 
-                                                                                                        dialog.dismiss();
-                                                                                                        Intent b = new Intent(CustomerPayment.this, CustomerFoodPanel_BottomNavigation.class);
-                                                                                                        b.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                                                                                        startActivity(b);
-                                                                                                        finish();
-
-                                                                                                    }
-                                                                                                });
-                                                                                                AlertDialog alert = builder.create();
-                                                                                                alert.show();
-                                                                                            }
                                                                                         });
                                                                                     }
                                                                                 });
@@ -219,25 +272,43 @@ public class CustomerPayment extends AppCompatActivity {
     }
 
 
-    private void sendNotifications(String usertoken, String title, String message, String confirm) {
-        Data data = new Data(title, message, confirm);
-        NotificationSender sender = new NotificationSender(data, usertoken);
-        apiService.sendNotification(sender).enqueue(new Callback<MyResponse>() {
+    private void sendNotification(String messageBody){
+        ApiClient.getClient().create(ApiService.class).sendMessage(
+                Constants.getRemoteMsgHeaders(),
+                messageBody
+        ).enqueue(new Callback<String>() {
             @Override
-            public void onResponse(Call<MyResponse> call, Response<MyResponse> response) {
-                if (response.code() == 200) {
-                    if (response.body().success != 1) {
-                        Toast.makeText(CustomerPayment.this, "Failed", Toast.LENGTH_SHORT).show();
+            public void onResponse(@NonNull Call<String> call, @NonNull Response<String> response) {
+                if(response.isSuccessful()){
+                    try{
+                        if(response.body() != null){
+                            JSONObject responseJson = new JSONObject(response.body());
+                            JSONArray results = responseJson.getJSONArray("result");
+                            if(responseJson.getInt("failure") == 1){
+                                JSONObject error = (JSONObject) results.get(0);
+                                Toast.makeText(CustomerPayment.this, "Error.", Toast.LENGTH_LONG).show();
+                                return;
+                            }
+                        }
+                    }catch(JSONException e){
+                        e.printStackTrace();
                     }
+                    //  showToast("Notification sent successfully");
+
+                }else{
+                    Toast.makeText(CustomerPayment.this, "Error.", Toast.LENGTH_LONG).show();
+
                 }
             }
-
             @Override
-            public void onFailure(Call<MyResponse> call, Throwable t) {
-
+            public void onFailure(@NonNull Call<String> call, @NonNull Throwable t) {
+                Toast.makeText(CustomerPayment.this, "Error.", Toast.LENGTH_LONG).show();
             }
         });
+
     }
+
 }
+
 
 
